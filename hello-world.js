c83598a762aea68c93515d646324237f3c46486a
genis99 <gvil7492@gmail.com>
2019-09-19 20:57:36 +0200

    Missatge

--- hello-world.js
+++ hello-world.js
@@ -2,7 +2,7 @@
 // var greeting = 'Hola mon'
 
 //Alternatives Javascript modern
-// const greeting = 'Hola mon'
+const greeting = 'Hola mon'
 // let greeting = 'Hola mon'
 
 //Scope (zona en la que es pot utilitzar aquella variable)
--- hello-world.php
+++ hello-world.php
@@ -1,3 +1,11 @@
 <?php
 $greeting = "Hola";
 echo $greeting; //sdasd
+
+$name = 'Genis';
+//Compte en js és + per concatenar
+echo 'Hola ' . $greeting;
+//Posar normalment cometes simples perque:
+echo 'Hola $greeting';
+echo "Hola $greeting";
+//Les dobles si que t'agafen la variable
